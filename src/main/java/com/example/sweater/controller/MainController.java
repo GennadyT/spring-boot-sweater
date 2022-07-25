8f68c804f329cff3db211a3bc182b9caa8d61ee4
GennadyT <gennadytarasov9@gmail.com>
2022-07-25 16:04:39 +0300

    renamed:    src/main/resources/templates/greeting.ftlh -> src/main/resources/templates/greeting.ftl         new file:   src/main/resources/templates/login.ftl         deleted:    src/main/resources/templates/login.mustache         renamed:    src/main/resources/templates/main.mustache -> src/main/resources/templates/main.ftl         new file:   src/main/resources/templates/registration.ftl         deleted:    src/main/resources/templates/registration.mustache

--- src/main/java/com/example/sweater/controller/MainController.java
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -20,7 +19,7 @@ public class MainController {
 
     @GetMapping("/")
     public String greeting(Map<String,Object> model){
-        return "greeting";
+        return "greeting.ftl";
     }
 
     @GetMapping("/main")
