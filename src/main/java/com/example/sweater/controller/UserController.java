84234ba88b2fae4a6f934b705a3fb32cc3bc23f0
GennadyT <gennadytarasov9@gmail.com>
2022-07-26 10:42:29 +0300

    created userList.ftl & modified UserController

--- src/main/java/com/example/sweater/controller/UserController.java
+++ src/main/java/com/example/sweater/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.sweater.controller;
 import com.example.sweater.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,7 +14,8 @@ public class UserController {
     private UserRepo userRepo;
 
     @GetMapping
-    public String userList(){
+    public String userList(Model model){
+        model.addAttribute("user", userRepo.findAll());
         return "userList";
     }
 }
