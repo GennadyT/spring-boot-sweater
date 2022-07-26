e1cf090bcb5952155a84e549ac65728bebff8a42
GennadyT <gennadytarasov9@gmail.com>
2022-07-26 18:51:55 +0300

    modif. MvcConfig & MainController

--- src/main/java/com/example/sweater/controller/MainController.java
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -66,7 +66,7 @@ public class MainController {
             String uuidFile = UUID.randomUUID().toString();
             String resultFilename = uuidFile + "." + file.getOriginalFilename();
 
-            file.transferTo(new File(resultFilename));
+            file.transferTo(new File(uploadPath + "/" + resultFilename));
 
             message.setFilename(resultFilename);
         }
