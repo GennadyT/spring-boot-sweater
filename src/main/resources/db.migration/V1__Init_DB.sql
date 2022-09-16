1ca1c7770afe1b1c4efc77e30141878b3d32ed70
GennadyT <gennadytarasov9@gmail.com>
2022-09-16 11:37:30 +0300

    modified:   src/main/resources/application.properties         modified:   src/main/resources/db.migration/V1__Init_DB.sql

--- src/main/resources/db.migration/V1__Init_DB.sql
+++ src/main/resources/db.migration/V1__Init_DB.sql
@@ -4,7 +4,7 @@ create table message (
     id int8 not null,
     filename varchar(255),
     tag varchar(255),
-    text varchar(255),
+    text varchar(2048) not null,
     user_id int8,
     primary key (id)
 );
@@ -19,14 +19,14 @@ create table usr (
     activation_code varchar(255),
     active boolean not null,
     email varchar(255),
-    password varchar(255),
-    username varchar(255),
+    password varchar(255) not null,
+    username varchar(255) not null,
 primary key (id)
 );
 
 alter table if exists message
-    add constraint FK70bv6o4exfe3fbrho7nuotopf
+    add constraint message_user_fk
     foreign key (user_id) references usr;
 alter table if exists user_role
-    add constraint FKfpm8swft53ulq2hl11yplpr5
+    add constraint user_role_user_fk
     foreign key (user_id) references usr;
